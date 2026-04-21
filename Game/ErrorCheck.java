d7008478a6fe3795c490c6cc84d15274377bc12f
rroy8000 <rohan_roy@horacemann.org>
2026-04-21 19:20:24 -0400

    implemented error check

--- Game/ErrorCheck.java
+++ Game/ErrorCheck.java
@@ -4,7 +4,24 @@ import java.util.Scanner;
 
 public class ErrorCheck {
 
+    public static int ans = 0;
+
     public static int getInt(Scanner sc) {
-        return sc.nextInt(); // add error checking here!
+
+        int ans;
+
+        while (true) {
+            while (!sc.hasNextInt()) {
+                System.out.print("Please enter a number that corresponds with a game");
+                sc.next();
+            }
+            ans = sc.nextInt();
+            if (ans >= 1 && ans <= 3) {
+                return ans;
+            } else {
+                System.out.print("Please enter a number that corresponds with a game");
+            }
+        }
+
     }
 }
